f03693076c9ce67c52e326164400cb0736662cda
PiromanV <84721213+PiromanV@users.noreply.github.com>
2024-09-18 17:14:23 +0300

    feh dmenu

--- soft.sh
+++ soft.sh
@@ -3,7 +3,7 @@
 # i3wm
 pacman -S i3-gaps lightdm xorg xorg-xrandr xorg-xinit polybar
 pacman -S picom lxappearance lightdm-gtk-greeter
-cp ./i3/config ~/.config/i3/config # copying my config
+cp ./i3/config /home/PiromanV/.config/i3/config # copying my config
 
 systemctl enable lightdm 
 
@@ -11,4 +11,4 @@ systemctl enable lightdm
 pacman -S ttf-jetbrains-mono-nerd materia-gtk-theme papirus-icon-theme
 
 # other
-pacman -S thunar firefox terminator neovim code qbittorrent obs-studio telegram-desktop maim xclip
+pacman -S thunar firefox terminator neovim code qbittorrent obs-studio telegram-desktop maim xclip dmenu feh
