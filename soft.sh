7737f8b517d0bacc1e3f70c7248a8e87f5e76ac1
PiromanV <84721213+PiromanV@users.noreply.github.com>
2024-09-18 16:35:18 +0300

    Add files via upload
    small changes

--- soft.sh
+++ soft.sh
@@ -2,12 +2,13 @@
 
 # i3wm
 pacman -S i3-gaps lightdm xorg xorg-xrandr xorg-xinit polybar
+pacman -S picom lxappearance lightdm-gtk-greeter
 cp ./i3/config ~/.config/i3/config # copying my config
 
 systemctl enable lightdm 
 
 # font
-pacman -S ttf-jetbrains-mono-nerd
+pacman -S ttf-jetbrains-mono-nerd materia-gtk-theme papirus-icon-theme
 
 # other
-pacman -S thunar firefox terminator neovim code qbittorrent steam obs-studio telegram-desktop maim xclip
+pacman -S thunar firefox terminator neovim code qbittorrent obs-studio telegram-desktop maim xclip
