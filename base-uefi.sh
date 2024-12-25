c7786e48831f69be8fe5826d24d2da6cc6b74735
PiromanV <vladimirvargasov@yandex.ru>
2024-12-25 15:43:11 +0300

    fixed locales

--- base-uefi.sh
+++ base-uefi.sh
@@ -2,7 +2,7 @@
 
 ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 hwclock --systohc
-sed -i '178s/.//' /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "arch" >> /etc/hostname
